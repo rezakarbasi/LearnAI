399962607d563983802c012790fcd909121282cf
HajReza <a.rz.karbasi@gmail.com>
2019-08-22 11:40:28 +0430

    shape and random function had been tested

--- Numpy/DataTypes.py
+++ Numpy/DataTypes.py
@@ -5,3 +5,6 @@ b = np.array([[2, 2], [1, 0]])
 
 print('\ndot function product two array mathematically\n', a.dot(b))
 print('\n\nand * operator products each element of array separately\n', a*b)
+
+print('\n\nshape of the array is ', np.shape(a))
+print('\n\nnp.random.random(()) makes a random array : \n', np.random.random((2, 3)))
