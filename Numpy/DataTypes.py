ef8e851d3531872f695b8a74748126f8f5f512df
HajReza <a.rz.karbasi@gmail.com>
2019-08-22 11:24:29 +0430

    diffrence between dot function and * operator

---
+++ Numpy/DataTypes.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([[0, 1], [2, 3]])
+b = np.array([[2, 2], [1, 0]])
+
+print('\ndot function product two array mathematically\n', a.dot(b))
+print('\n\nand * operator products each element of array separately\n', a*b)
