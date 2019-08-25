27041886a8a2c906d0a17c21d93b034bb86ab9b3
HajReza <a.rz.karbasi@gmail.com>
2019-08-26 00:01:04 +0430

    regularization code added - but previous commit was cleaner

--- tensorflow/FirstNetwork.py
+++ tensorflow/FirstNetwork.py
@@ -11,15 +11,15 @@ import matplotlib.pyplot as plt
 class_names = ['0', '1', '2', '3', '4',
                '5', '6', '7', '8', '9']
 
-plt.figure(figsize=(10, 10))
-for i in range(25):
-    plt.subplot(5, 5, i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(tr_im[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[tr_label[i]])
-plt.show()
+# plt.figure(figsize=(10, 10))
+# for i in range(25):
+#     plt.subplot(5, 5, i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(tr_im[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[tr_label[i]])
+# plt.show()
 
 # preprocess the data
 
@@ -33,12 +33,12 @@ test_im = test_im.astype('float32')/255
 
 # second : to use softmax classification we shold have n*10 array then categorize labels too
 train_label = ku.to_categorical(tr_label)
-test_label = ku.to_categorical(te_label)
+test_label = ku.to_categorical(te_plot(history.history)plot(history.history)label)
 
 # bulid the model from sequential
 nn = models.Sequential()
 # add first layer and configure hidden layer to 512 neurons and set it's activation function
-nn.add(layers.Dense(512, activation='relu', input_shape=(28*28,)))
+nn.add(layers.Dense(512, activation='relu', input_shape=(28*28,),kernel_regularizer=regularizers.l2(0.01)))
 # add final layer and set it to softmax layer
 nn.add(layers.Dense(10, activation='softmax'))
 
@@ -47,7 +47,8 @@ nn.compile(optimizer='rmsprop', loss='categorical_crossentropy',
            metrics=['accuracy'])
 
 # finally the fit function
-nn.fit(train_im, train_label, epochs=5, batch_size=120)
+history=nn.fit(train_im, train_label, epochs=5, batch_size=120)
 
 # in the end evaluate the trained network by test dataset
 print(nn.evaluate(test_im, test_label))
+# plot(history.)
